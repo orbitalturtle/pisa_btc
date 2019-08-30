78cc06377724ff58a164e4192f38469205df65bd
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2019-08-30 12:57:21 +0200

    Adds classmethod to construct appointments from json

--- pisa/appointment.py
+++ pisa/appointment.py
@@ -12,6 +12,13 @@ class Appointment:
         self.cipher = cipher
         self.hash_function = hash_function
 
+    @classmethod
+    def from_json(cls, json_appointment):
+        return cls(json_appointment.get("locator"), json_appointment.get("start_time"),
+                   json_appointment.get("end_time"), json_appointment.get("dispute_delta"),
+                   json_appointment.get("encrypted_blob"), json_appointment.get("cipher"),
+                   json_appointment.get("hash_function"))
+
     def to_json(self):
         appointment = {"locator": self.locator, "start_time": self.start_time, "end_time": self.end_time,
                        "dispute_delta": self.dispute_delta, "encrypted_blob": self.encrypted_blob.data,
