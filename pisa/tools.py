0b43fdcfc157d070ad0970ca89d0ff7d1dbdad2e
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2019-09-03 15:35:01 +0200

    WIP handle reorgs while offline

--- pisa/tools.py
+++ pisa/tools.py
@@ -1,4 +1,5 @@
 import re
+from pisa.appointment import Appointment
 from pisa.utils.authproxy import JSONRPCException
 from pisa.rpc_errors import RPC_INVALID_ADDRESS_OR_KEY
 from http.client import HTTPException
@@ -55,6 +56,60 @@ def in_correct_network(bitcoin_cli, network):
         correct_network = True
 
     return correct_network
+
+
+def find_last_common_block(bitcoin_cli, last_known_blocks, debug, logging):
+    last_common_block = None
+
+    for block_hash in last_known_blocks:
+        try:
+            bitcoin_cli.getblock(block_hash)
+            last_common_block = block_hash
+
+        except JSONRPCException as e:
+            if e.error.get('code') == RPC_INVALID_ADDRESS_OR_KEY:
+                if debug:
+                    if block_hash == last_known_blocks[-1]:
+                        logging.info('[Pisad] cannot bootstrap from backed up data, no common block found')
+
+                    else:
+                        logging.info('[Pisad] block {} not found. Backtracking...')
+
+    return last_common_block
+
+
+def rewind_states(bitcoin_cli, watcher_appointments, responder_jobs):
+    # There's nothing to be done in terms of updating nor moving appointments from the watcher to the responder.
+    # Once the watcher is bootstrapped with the missed blocks and the old state it will be able to handle the changes.
+    # However, some jobs may need to go back to the watcher in case of a reorg taking them out of the chain.
+
+    for uuid, job in responder_jobs:
+        try:
+            tx_info = bitcoin_cli.getrawtransaction(job.justice_txid, 1)
+            job.confirmations = int(tx_info.get("confirmations"))
+
+        except JSONRPCException as e:
+            if e.error.get('code') == RPC_INVALID_ADDRESS_OR_KEY:
+                # FIXME: could this overwrite some watcher's data?
+                # FIXME: we are missing most of the data since jobs were not supposed to go back
+                watcher_appointments[uuid] = Appointment(job.locator, None, job.appointment_end, None, None, None, None)
+                responder_jobs.pop(uuid)
+
+    return watcher_appointments, responder_jobs
+
+
+def get_missed_blocks(bitcoin_cli, last_common_block):
+    current_block_hash = bitcoin_cli.getbestblockhash()
+
+    missed_blocks = []
+
+    while current_block_hash != last_common_block and current_block_hash is not None:
+        missed_blocks.append(current_block_hash)
+
+        current_block = bitcoin_cli.getblock(current_block_hash)
+        current_block_hash = current_block.get("previousblockhash")
+
+    return missed_blocks[::-1]
 
 
 def check_txid_format(txid):
